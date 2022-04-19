28c53a722ee7bd9b7c6c3826d337e65a98e504c9
isaïe clarin <74413605+isaieCLARIN@users.noreply.github.com>
2022-04-18 21:20:08 -0400

    Add files via upload

---
+++ CalculatriceApp.swift
@@ -0,0 +1,19 @@
+//
+//  CalculatriceApp.swift
+//  Calculatrice
+//
+//  Created by Isaïe Clarin on 03/12/2021.
+//
+
+import SwiftUI
+import UIKit
+
+@main
+struct CalculatriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+//			CalculatriceViewController()
+        }
+    }
+}
